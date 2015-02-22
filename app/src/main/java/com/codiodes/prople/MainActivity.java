3fc15ab8a70a0f3dc4e81d4f105f0fa07c831a47
codiodes <info@codiodes.com>
2015-02-21 22:52:31 -0800

    Added code to show full-screen splash page during first launch of app.

--- app/src/main/java/com/codiodes/prople/MainActivity.java
+++ app/src/main/java/com/codiodes/prople/MainActivity.java
@@ -1,22 +1,41 @@
 package com.codiodes.prople;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.codiodes.prople.Helper.Constants;
 
 
 public class MainActivity extends ActionBarActivity {
 
     Intent intent;
+    private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  Show the splash screen.
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(MainActivity.this, LandingPage.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 
 
